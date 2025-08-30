a1849c85b1ea69e93a516920d8784a8a0a7bc5a5
r006 <79533648+r006@users.noreply.github.com>
2025-08-30 05:30:31 -0400

    Used 1 loop to make code more efficient

--- algorithms/strings/valid_palindrome.cpp
+++ algorithms/strings/valid_palindrome.cpp
@@ -4,6 +4,19 @@ Clean string and check if it reads the same forward and backward.
 
 2 pointer question
 
+--------- 1 Loop --------
+    Declare 2 pointers front = 0, back = size of array - 1
+    While back > front (converge to midpoint of array)
+    Skip any non alphanumeric characters
+    Convert  character to lowercase using tolower() C++ function and compare if characters are the same 
+    if characters are not same, return false 
+    if charcters are same,increment front, decrement back 
+    return true
+        Loop through each character in string
+   
+
+---- 2 Loops not efficent-----
+
 Clean string function:
     Loop through each character in string
     Remove any non alphanumeric characters using !isalnum() (#include <cctype>) and erase(position, number to erase) C++ function
@@ -15,7 +28,7 @@ isPalindrome fucntion:
     if characters are not same, return false 
     if charcters are same,increment front, decrement back 
     return true
-*/
+
 
 class Solution {
 public:
@@ -45,5 +58,28 @@ public:
         }
         return true;
 
+    }
+};
+*/
+
+class Solution {
+public:
+    bool isPalindrome(string s) { 
+        int front = 0, back = s.length() - 1; 
+        while (back > front){
+            if (!isalnum(s[front])) {
+                front ++;
+            } 
+            else if (!isalnum(s[back]) ) {
+                back--;
+            } else {
+                if (tolower(s[front]) != tolower(s[back])){
+                    return false;
+                }  
+                front++;
+                back --;
+            }
+        }
+        return true;
     }
 };
