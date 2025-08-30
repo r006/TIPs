21f9e76de5362cb342b6cb177749bdc88301b236
r006 <rajeshsharma06@yahoo.com>
2025-08-30 18:42:03 -0400

    Add 5 UMP string problems (C++) with pseudocode

---
+++ algorithms/strings/reverse_string.cpp
@@ -0,0 +1,28 @@
+/*
+
+Reverse String
+
+Utilize 2 pointer approach and swap function
+
+Declare 2 pointers front = 0, back = size of array - 1
+While back > front (converge to midpoint of array), swap s[front] and s[back]
+increment front, decrement back
+return s
+
+
+*/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int front = 0;
+        int back = s.size()-1;
+
+        while (back > front){
+            swap(s[front],s[back]);
+            front++;
+            back --;
+        }
+
+    }
+};
