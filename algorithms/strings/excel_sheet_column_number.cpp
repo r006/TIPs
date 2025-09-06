2987c4fcef5048716b540aad425506058bc8ed99
r006 <rajeshsharma06@yahoo.com>
2025-09-06 13:16:57 -0400

    add edexcel_sheet_column_title.cpp

---
+++ algorithms/strings/excel_sheet_column_number.cpp
@@ -0,0 +1,22 @@
+/*
+
+Convert title to interger based on A = 1, z = 26
+
+Initialize int result to 0.
+Loop through each character in the string 
+    Convert the character to number .
+    Multiply result by 26.
+    Add the numeric value of the current character to result.
+
+Return result.
+
+
+*/
+
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        
+    }
+};
